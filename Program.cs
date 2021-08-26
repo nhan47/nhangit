2948f91c04ddcdb48a6fd9f7af4aa0d65c5c48d0
Thanh Nhan <thanhnhanstudy@gmail.com>
2021-08-26 11:40:12 +0700

    Comment the AgeCalculator function

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            ushort year;
+
+            name = Console.ReadLine();
+            year = Convert.ToUInt16(Console.ReadLine());
+
+            System.Console.WriteLine("Name : {0}, year : {1}", name, year);
+        }
+
+        private static ushort AgeCalculator(ushort bornYear, ushort yearToCalculate)
+        {
+            return Convert.ToUInt16((int)yearToCalculate - (int)bornYear);
+        }
+    }
+}
