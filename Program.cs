ed2193f14e5e0cf94fc606e7def3aae8e4be9923
nhan47 <85204685+nhan47@users.noreply.github.com>
2021-08-27 16:13:19 +0700

    Change program to day calculate

--- Program.cs
+++ Program.cs
@@ -6,20 +6,10 @@ namespace Git
     {
         static void Main(string[] args)
         {
-            string name;
-            ushort year;
-
-            name = Console.ReadLine();
-            year = Convert.ToUInt16(Console.ReadLine());
-
-            System.Console.WriteLine("Name : {0}, year : {1}", name, year);
-            System.Console.WriteLine("You're {0} years old", AgeCalculator(year, 2021));
-            System.Console.WriteLine("You will be {0} in 2025!", AgeCalculator(year, 2025));
+            DateTime startDay = new DateTime(2019, 1, 29);
+            DateTime endDate = DateTime.Today;
+            Console.WriteLine((endDate - startDay).TotalDays);
         }
 
-        private static ushort AgeCalculator(ushort bornYear, ushort yearToCalculate)
-        {
-            return Convert.ToUInt16((int)yearToCalculate - (int)bornYear);
-        }
     }
 }
